c5d918e7a988fcc3e2581cf5cb193322bf14de62
Siaukovich Kiryl <k.sevkovich@gmail.com>
2018-08-05 00:57:23 +0300

    ParserService was updated to match new project architecture.

--- UrlToXml/ParserService.cs
+++ UrlToXml/ParserService.cs
@@ -24,50 +24,43 @@
         /// <param name="consumer">
         /// Data consumer.
         /// </param>
-        /// <param name="validator">
-        /// Data validator.
+        /// <param name="mapper">
+        /// Data mapper.
         /// </param>
-        /// <param name="parser">
-        /// Data parser.
+        /// <param name="logger">
+        /// Logger.
         /// </param>
         public void Parse(
             IDataProvider<TInitial> provider, 
             IDataConsumer<TTarget> consumer, 
-            IGroupDataValidator<TInitial> validator, 
-            IDataParser<TInitial, TTarget> parser)
+            IMapper<TInitial, TTarget> mapper,
+            ILogger logger = null)
         {
+            logger?.Info("ParserService begun to work");
+
+            if (provider == null)
+            {
+                throw new ArgumentNullException(nameof(provider));
+            }
+
+            if (consumer == null)
+            {
+                throw new ArgumentNullException(nameof(consumer));
+            }
+
+            if (mapper == null)
+            {
+                throw new ArgumentNullException(nameof(mapper));
+            }
+
             string source = ConfigurationManager.AppSettings["source"];
             string destination = ConfigurationManager.AppSettings["destination"];
 
             IEnumerable<TInitial> data = provider.ProvideData(source);
 
-            IEnumerable<TTarget> parsedData = ParseData(data, parser, validator);
-            
+            IEnumerable<TTarget> parsedData = mapper.Map(data, logger);
+
             consumer.Consume(parsedData, destination);
-        }
-
-        private IEnumerable<TTarget> ParseData(IEnumerable<TInitial> data, IDataParser<TInitial, TTarget> parser, IGroupDataValidator<TInitial> groupValidator)
-        {
-            int elementNumber = 1;
-            foreach (TInitial dataElement in data)
-            {
-                TTarget t;
-                try
-                {
-                    t = parser.Parse(dataElement, groupValidator);
-                }
-                catch (ArgumentException ex)
-                {
-                    LoggerService.Instance.Logger.Warning($"Invalid data in line #{elementNumber}. {ex.Message}");
-                    continue;
-                }
-                finally
-                {
-                    elementNumber++;
-                }
-
-                yield return t;
-            }
         }
     }
 }
