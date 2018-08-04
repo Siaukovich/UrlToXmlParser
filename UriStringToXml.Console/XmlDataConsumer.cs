4cd2969dd9438933e080947bb0b30359ea73b675
Siaukovich Kiryl <k.sevkovich@gmail.com>
2018-08-04 18:27:12 +0300

    xmlDataConsumer was done.

--- UriStringToXml.Console/XmlDataConsumer.cs
+++ UriStringToXml.Console/XmlDataConsumer.cs
@@ -2,6 +2,7 @@
 {
     using System;
     using System.Collections.Generic;
+    using System.Xml.Linq;
 
     using UrlToXml.Interfaces;
 
@@ -30,7 +31,63 @@
 
         public void Consume(IEnumerable<Url> data, string destination)
         {
-            throw new NotImplementedException();
+            var rootElement = new XElement("urlAddresses");
+
+            foreach (Url url in data)
+            {
+                XElement hostName = GetHostNameXElement(url);
+                XElement uriXElement = GetUriSegmentsXElement(url);
+                XElement parametersXElement = GetParametersXElement(url);
+
+                XElement urlAddress = ComposeUrlAddressXElement(hostName, uriXElement, parametersXElement);
+
+                rootElement.Add(urlAddress);
+            }
+
+            rootElement.Save(destination);
+        }
+
+        private static XElement ComposeUrlAddressXElement(XElement hostName, XElement uriXElement, XElement parametersXElement)
+        {
+            return new XElement("urlAdress",
+                                hostName,
+                                uriXElement,
+                                parametersXElement);
+        }
+
+        private XElement GetHostNameXElement(Url url)
+        {
+            return new XElement("host", new XAttribute("name", url.Host));
+        }
+
+        private static XElement GetUriSegmentsXElement(Url url)
+        {
+            string[] uriSegements = url.UrlPath.Split(new[] { '/' }, StringSplitOptions.RemoveEmptyEntries);
+            var uriXElement = new XElement("uri");
+            foreach (string uriSegement in uriSegements)
+            {
+                uriXElement.Add(new XElement("segment", uriSegement));
+            }
+
+            return uriXElement;
+        }
+
+        private static XElement GetParametersXElement(Url url)
+        {
+            var parametersXElement = new XElement("parameters");
+
+            if (url.Parameters.Count == 0)
+            {
+                return null;
+            }
+
+            foreach ((string key, string value) parameterPair in url.Parameters)
+            {
+                parametersXElement.Add(new XElement("parameter", new XAttribute("key", parameterPair.key),
+                    new XAttribute("value", parameterPair.value)));
+            }
+
+            return parametersXElement;
         }
     }
 }
